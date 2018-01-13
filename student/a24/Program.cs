cd4b86bad4f8ac95dba17d31603ddedfc1e5d3f6
aigrimas <aigerimka9914@mail.ru>
2018-01-13 21:16:23 +0600

    student

---
+++ student/a24/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a24
+{
+	class Student
+	{
+		public string  firstName;
+        public string secondName;
+		public int age;
+		public float gpa;
+
+		public Student()
+		{
+			firstName = "Dana";
+			secondName = "Kurmash";
+			age = 18;
+			gpa = 4;
+
+		}
+		public override string ToString()
+		{
+			return firstName + " "+ secondName + " " + age + " " + gpa;
+		}
+	
+		static void Main(string[] args)
+		{
+			Student s = new Student();
+			s.firstName = "Dos";
+
+		
+			Console.WriteLine(s);
+			Console.ReadKey();
+
+
+		}
+		
+	}
+}
