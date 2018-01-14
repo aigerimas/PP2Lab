98b71b9bd22c5a5ee357c6a98c591b4aca3415f6
aigrimas <aigerimka9914@mail.ru>
2018-01-14 16:50:19 +0600

    rectangle

---
+++ rectangle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a34
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Rectangle x = new Rectangle();
+			x.findarea();
+			x.findperimeter();
+			Console.WriteLine(x);
+			Console.ReadKey();
+		}
+	}
+}
---
+++ rectangle/Rectangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a34
+{
+	class Rectangle
+	{
+		public   int width;
+		public int height;
+		public int area;
+		public int perimeter;
+		
+		public Rectangle()
+		{
+			width = 5;
+			height = 4;
+		}
+		public Rectangle( int width, int height)
+		{
+			this.width = width;
+			this.height = height;
+		}
+		public void findarea()
+		{
+			area = width * height;
+		}
+		public void findperimeter()
+		{
+			perimeter = (width + height) * 2;
+		}
+		public override string ToString()
+		{
+			return area + " " + perimeter;
+		}
+
+	}
+	
+	
+	
+}
