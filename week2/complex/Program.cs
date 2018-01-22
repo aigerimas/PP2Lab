c6ff15ea899dfcbc4b73bbb5ff82fecc5859985e
aigrimas <aigerimka9914@mail.ru>
2018-01-23 00:10:15 +0600

    week2

---
+++ week2/complex/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a37
+{
+	class Program
+	{
+		
+
+		static void Main(string[] args)
+		{
+			Complex c1 = new Complex(1, 4);
+			Complex c2 = new Complex(1, 2);
+			Complex result = c1 + c2;
+			
+			
+			Console.WriteLine(result);
+			Console.ReadKey();
+		}
+	}
+}
---
+++ week2/max,min/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a39
+{
+	class Program
+	{
+		  static void Main(string[] args)
+		{
+			FileStream fs = new FileStream(@"C:\Users\User\Desktop\input.txt", FileMode.Open, FileAccess.Read);
+			StreamReader sr = new StreamReader(fs);
+
+			string s = sr.ReadLine();
+			
+			string[] arr = s.Split(' '); 
+			int maxi = int.Parse(arr[0]); 
+			int mini = int.Parse(arr[0]); 
+			for (int i = 1; i <arr.Length; i++)
+			{
+
+				if (int.Parse(arr[i]) > maxi)
+				{
+					maxi = int.Parse(arr[i]);
+				}
+				if (int.Parse(arr[i]) < mini)
+					mini = int.Parse(arr[i]);
+			}
+			sr.Close();
+			fs.Close();
+			Console.WriteLine(maxi);
+			Console.WriteLine(mini);
+				Console.ReadKey();
+			        
+
+			}
+		}
+	}
+
