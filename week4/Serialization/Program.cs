678911b362a39c4ccb3ee546af5424f444899de6
aigrimas <aigerimka9914@mail.ru>
2018-02-06 09:23:35 +0600

    week4

---
+++ week4/Serialization/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+	class Program
+	{
+		static void f1()
+		{
+			FileStream fs = new FileStream(@"data.ser", FileMode.Create, FileAccess.Write);
+			Complex c1 = new Complex(1,4);
+			
+			BinaryFormatter bf = new BinaryFormatter();
+			try
+			{
+				bf.Serialize(fs, c1);
+				
+			}
+			catch(Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+			finally
+			{
+				fs.Close();
+
+			}
+			Console.WriteLine("done");
+		}
+		static void f2()
+		{
+			FileStream fs = new FileStream(@"data.ser", FileMode.Open, FileAccess.Read);
+			BinaryFormatter bf = new BinaryFormatter();
+
+			try
+			{
+				Complex c = bf.Deserialize(fs) as Complex;
+				Console.WriteLine(c);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+			finally
+			{
+				fs.Close();
+			}
+		}
+		static void Main(string[] args)
+		{
+			f1();
+			f2();
+			Console.ReadKey();
+		}
+	}
+}
---
+++ week4/Serialization/Serialization.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+	[Serializable]
+	class Complex
+	{
+		public int a;
+		public int b;
+		public int GCD(int x, int y)
+		{
+			while (x != y)
+			{
+				if (x > y)
+				{
+					x = x - y;
+
+				}
+				else
+				{
+					y = y - x;
+				}
+			}
+			return x;
+		}
+		public Complex()
+		{
+
+		}
+		public Complex(int ax,int bx)
+		{
+			a = ax;
+			b = bx;
+		}
+		public void Simp()
+		{
+			int z = GCD(this.a, this.b);
+			this.a = this.a / z;
+			this.b = this.b / z;
+		}
+		public static Complex operator +(Complex c1,Complex c2)
+		{
+			Complex m = new Complex();
+			m.a = c1.a * c2.b + c2.a * c1.b;
+			m.b = c1.b * c2.b;
+			m.Simp();
+			return m;
+
+		}
+		public override string ToString()
+		{
+			return a + "/" + b;
+			
+		}
+	}
+}
